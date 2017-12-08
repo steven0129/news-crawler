6a95ccd93fb13d4b3619a5f01fcd2da38f3ae73b
Administrator <h3584935@gmail.com>
2017-12-08 21:55:55 +0800

    news-crawler v0.0.1 complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import time
+import csv
 from selenium import webdriver
 from pyvirtualdisplay import Display
 
@@ -11,6 +12,7 @@ driver = webdriver.Firefox()
 def listArticle(driver, page):
     result = []
     driver.get('http://news.ltn.com.tw/list/breakingnews/all/' + str(page))
+
     for elem in driver.find_elements_by_class_name('tit'):
         result.append({'title': elem.find_element_by_tag_name(
             'p').text, 'url': elem.get_attribute('href')})
@@ -26,15 +28,23 @@ def articleContent(driver, url):
 
 driver.maximize_window()
 
-temp = []
-for index, article in enumerate(listArticle(driver, 1)):
+for page in range(1, 39):
+    print('------------------Page' + str(page) + '-------------------')
     try:
-        time.sleep(random.uniform(0, 2))
-        article['id'] = index + 1
-        article['content'] = articleContent(driver, article['url'])
-        temp.append(article)
-        print(temp)
+        for index, article in enumerate(listArticle(driver, page)):
+            try:
+                time.sleep(random.uniform(0, 10))
+                article['content'] = articleContent(driver, article['url'])
+
+                with open('news.csv', 'a') as f:
+                    fCSV = csv.writer(f)
+                    temp = [article['title'], article['url'],article['content'].replace('\n', '<parp>')]
+                    fCSV.writerow(temp)
+
+                print('article index ' + str(index) + ' finish')   
+            except:
+                print('error at index ' + str(index))
     except:
-        print('error at index ' + str(index))
+        print('listArticle() error!')
 
 driver.quit()
